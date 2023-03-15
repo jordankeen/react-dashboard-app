3f3dcf1627231add7ba733f8699f79f5ed9c7969
Jordan Keen <jordankeen.7@gmail.com>
2023-03-14 20:53:49 -0400

    created calendar page with full calendar

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Team from './scenes/team';
 import Contacts from "./scenes/contacts";
 import Invoices from './scenes/invoices';
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar";
 
 function App() {
    const [theme, colorMode] = useMode();
@@ -38,7 +38,7 @@ function App() {
                   {/* <Route path="/pie" element={<Pie />} /> */}
                   {/* <Route path="/line" element={<Line />} /> */}
                   {/* <Route path="/faq" element={<FAQ />} /> */}
-                  {/* <Route path="/calendar" element={<Calendar />} /> */}
+                  <Route path="/calendar" element={<Calendar />} />
                   {/* <Route path="/geography" element={<Geography />} /> */}
                </Routes>
             </main>
